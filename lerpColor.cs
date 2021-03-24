6ebffad306fc76e6a5a2ebb3a20d0aa2347fa57a
anwar zigler <77289226+Zigler1@users.noreply.github.com>
2021-03-24 23:31:29 +0100

    Add files via upload

---
+++ lerpColor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lerpColor : MonoBehaviour
+{
+    MeshRenderer mesh;
+    [SerializeField] Color[] mycolor;
+    [SerializeField] [Range(0f, 1f)] float lerptime;
+    float T;
+    int ln;
+    int colorinderx = 0;
+
+
+    void Start()
+    {
+        mesh = GetComponent<MeshRenderer>();
+        ln = mycolor.Length;
+    }
+
+    void Update()
+    {
+        mesh.material.color = Color.Lerp(mesh.material.color, mycolor[colorinderx], lerptime * Time.deltaTime);
+        T = Mathf.Lerp(T, 1f, lerptime * Time.deltaTime);
+        if (T > 0.9f)
+        {
+            T = 0f;
+            colorinderx++;
+            colorinderx = (colorinderx >= ln) ? 0 : colorinderx;
+        }
+
+    }
+}
